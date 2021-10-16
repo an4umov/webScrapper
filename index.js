ada8baa81bc6069e30ff9687258be37b2a909a7e
an4umov <an4umov@gmail.com>
2021-10-16 10:49:09 +0300

    +add scrapping from news page

--- index.js
+++ index.js
@@ -5,4 +5,24 @@ const express = require('express')
 
 const app = express()
 
+const url = 'https://www.landrover.ru/explore-land-rover/news/index.html'
+
+axios(url)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+
+        $('h2.ContentCard__heading', html).each(function() {
+            const title = $(this).text()
+            // const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                // url
+            })
+        })
+
+        console.log(articles)
+    }).catch(err => console.log(err))
+
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
